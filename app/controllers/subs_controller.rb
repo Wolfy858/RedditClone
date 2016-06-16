5e945c8295363eb484c12e2b92fda8b4a6c87722
Elliot Wolfe <email": null>
2016-06-16 15:14:33 -0700

    finished all CRUD routes for subs

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -12,8 +12,37 @@ get '/subs/new' do
   erb :'/subs/new'
 end
 
+post '/subs' do
+  @sub = Sub.create(params[:sub])
+  @sub.moderator = current_user
+
+  if @sub.valid?
+    redirect '/subs'
+  else
+    erb :'/subs/new'
+  end
+end
+
 get '/subs/:id' do
   @sub = Sub.find(params[:id])
   erb :"/subs/show"
+end
+
+get '/subs/:id/edit' do
+  @sub = Sub.find(params[:id])
+  redirect '/subs' unless current_user == @sub.moderator
+  erb :'/subs/edit'
+end
+
+patch "/subs/:id" do
+  @sub = Sub.find(params[:id])
+  redirect '/subs' unless current_user == @sub.moderator
+  @sub.update(params[:sub])
+
+  if @sub.valid?
+    redirect '/subs'
+  else
+    erb :"/subs/edit"
+  end
 end
 
