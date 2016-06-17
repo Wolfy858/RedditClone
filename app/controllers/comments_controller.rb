c101e6b105491c597c1e04e329b9310b2981436b
Michael Dumalag <email": null>
2016-06-16 17:50:40 -0700

    create comment partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ end
 
 post "/subs/posts/:post_id/comments" do
   post = Post.find(params[:post_id])
+  sub_id = post.author_id
   Comment.create!(commentable_id: post.id, commentable_type: 'Post', user_id: current_user.id, content: params[:content] )
-
+  redirect "/subs/#{sub_id}/posts/#{post}"
 end
+
