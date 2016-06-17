d92314b8fe010bd3853111c3ad1ff33346ad99a6
Elliot Wolfe <email": null>
2016-06-16 17:20:46 -0700

    Added routes and views to display individual posts on a subs page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-get 'subs/:id/posts/new' do
+get '/subs/:id/posts/new' do
   @sub = Sub.find(params[:id])
   @post = Post.new
   erb :'/posts/new'
@@ -10,10 +10,15 @@ post "/subs/:id/posts" do
   @post.author = current_user
   @post.sub = @sub
 
-  if @post.save?
+  if @post.save
     redirect "/subs/#{@sub.id}"
   else
     erb :'/posts/new'
   end
+end
+
+get "/subs/:sub_id/posts/:id" do
+  @post = Post.find(params[:id])
+  erb :"/posts/show"
 end
 
